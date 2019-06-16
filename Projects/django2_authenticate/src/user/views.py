84c75553de68012566d61110ba1a2b969b28816b
Hasan Khan <hasan.khan.uap@gmail.com>
2019-06-17 03:09:49 +0600

    rest api

--- Projects/django2_authenticate/src/user/views.py
+++ Projects/django2_authenticate/src/user/views.py
@@ -5,18 +5,47 @@ from django.contrib.auth import authenticate, login
 def index(request):
     return render(request, 'index.html')
 
+# def register(request):
+#     if request.method == 'POST':
+#         form = UserCreationForm(request.POST)
+#         if form.is_valid():
+#             form.save()
+#             username = form.cleaned_data['username']
+#             password = form.cleaned_data['password1']
+#             user = authenticate(username=username, password=password)
+#             login(request, user)
+#             return redirect('index')
+#     else:
+#         form = UserCreationForm()
+#     context = {'form': form}
+#     return render(request, 'registration/register.html', context)
+
 def register(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = ExtendedCreationForm(request.POST)
+        profile_form = UserProfileForm(request.POST)
+
         if form.is_valid():
-            form.save()
-            username = form.cleaned_data['username']
-            password = form.cleaned_data['password1']
-            user = authenticate(username=username, password=password)
+            user = form.save()
+
+            profile = profile_form.save(commit=False)
+            profile.user = user
+            profile.save()
+
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(
+                username=username,
+                password=password
+            )
             login(request, user)
             return redirect('index')
-    else:
-        form = UserCreationForm()
-    context = {'form': form}
-    return render(request, 'registration/register.html', context)
+        else:
+            form = ExtendedUserCreationForm()
+            profile_form = UserProfileForm() 
 
+        context = {
+            'form': form,
+            'profile_form': profile_form
+        }
+        return render(request, 'example/register.html', context)
